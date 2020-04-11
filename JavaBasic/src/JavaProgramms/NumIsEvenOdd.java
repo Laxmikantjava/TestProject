27b4e61e8423994d97cbfa18169ddf2532fed274
Laxmikant <Laxmikant@DESKTOP-RP9BL42>
2020-04-11 18:26:55 +0530

    second

---
+++ JavaBasic/src/JavaProgramms/NumIsEvenOdd.java
@@ -0,0 +1,18 @@
+package JavaProgramms;
+
+public class NumIsEvenOdd {
+
+	public static void main(String[] args) {
+		int  num = 8;
+		
+		{
+		if (num % 2 == 0) 
+			System.out.println("even no.");
+		
+		else 
+			System.out.println("odd no.");
+		
+	
+		}}
+
+}
