ee52d423b1a4466928eab93063edc2fc31f75bc8
Laxmikantjava <laxmikant211@gmail.com>
2020-06-04 21:01:59 +0530

    new

---
+++ JavaBasic/src/JavaBasicProgrammsPartTwo/CheckAlphabet.java
@@ -0,0 +1,16 @@
+package JavaBasicProgrammsPartTwo;
+
+public class CheckAlphabet {
+
+	public static void main(String[] args) {
+		
+		char d = '*';
+		if(d>='a' && d<='z') {
+			System.out.println(d+ "is alphabet");
+		}
+		else {
+			System.out.println(d+ "is not alphabet");
+		}
+	}
+
+}
