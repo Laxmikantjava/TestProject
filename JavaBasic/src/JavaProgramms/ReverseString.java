fde0cfd794cb3768346e38907b0a02b0d0964c71
Laxmikantjava <laxmikant211@gmail.com>
2020-06-07 20:53:50 +0530

    new change

--- JavaBasic/src/JavaProgramms/ReverseString.java
+++ JavaBasic/src/JavaProgramms/ReverseString.java
@@ -4,12 +4,16 @@ public class ReverseString {
 
 	public static void main(String[] args) {
 		
+		//reverse a string 
+		//difference in string and string buffer    string buffer is muttable
+		//do we have reverse function in string   No because string is immutable object
+		
 		String name = "deepak";
 		int length = name.length();
 		String rev ="";
 		for(int i=length-1;i>=0;i--) {
 			
-			rev= rev+name.charAt(i);
+			rev= rev+name.charAt(i); //return the char value at specified index
 		}
 		System.out.println("Reverse name is: "+rev);
 	}
